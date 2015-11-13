a1b2e5cdaaff08a1895cd12c9388be950c60e16b
Alvaro Garcia-Moya Herrera <agarcia@webtogo.de>
2015-11-13 09:35:55 +0100

    - Project. Update folder structure of the project

--- iBill/Classes/ViewController.m
+++ iBill/Classes/ViewController.m
@@ -9,19 +9,28 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *labelResult;
+@property (weak, nonatomic) IBOutlet UIButton *button;
+- (IBAction)buttonTapped:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UISlider *slider;
+@property (weak, nonatomic) IBOutlet UILabel *labelSlider;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)buttonTapped:(UIButton *)sender {
+}
 @end
